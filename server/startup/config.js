89b3be76e4734ed46a446594502d19afec476153
romaindepret91 <romaindepret91@gmail.com>
2022-12-06 11:31:23 -0500

    set up for heroku deployment: change config dir path (3)

--- server/startup/config.js
+++ server/startup/config.js
@@ -1,7 +1,7 @@
 // ----- SERVER CONFIGURATION -----
-process.env["NODE_CONFIG_DIR"] = "/server/config";
+const path = require("path");
+process.env.NODE_CONFIG_DIR = path.join(__dirname, "..", "config");
 const config = require("config");
-
 module.exports = function () {
   if (!config.get("jwtPrivateKey"))
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined");
